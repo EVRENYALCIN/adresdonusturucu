1b63cea85bbfce4d79efdb281a967b5bef1d2139
EVREN YALÇIN <103932803+EVRENYALCIN@users.noreply.github.com>
2023-03-08 00:15:08 +0300

    Add files via upload

---
+++ AdresDonusturucu/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _30_MetinCevirici
+{
+    partial class AdresDonusturucu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdresDonusturucu));
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtBuyukAdres = new System.Windows.Forms.TextBox();
+            this.txtKucukAdres = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(0, 2);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(577, 72);
+            this.txtAdres.TabIndex = 0;
+            this.txtAdres.TextChanged += new System.EventHandler(this.txtAdres_TextChanged);
+            // 
+            // txtBuyukAdres
+            // 
+            this.txtBuyukAdres.Location = new System.Drawing.Point(0, 73);
+            this.txtBuyukAdres.Multiline = true;
+            this.txtBuyukAdres.Name = "txtBuyukAdres";
+            this.txtBuyukAdres.Size = new System.Drawing.Size(577, 72);
+            this.txtBuyukAdres.TabIndex = 1;
+            // 
+            // txtKucukAdres
+            // 
+            this.txtKucukAdres.Location = new System.Drawing.Point(1, 144);
+            this.txtKucukAdres.Multiline = true;
+            this.txtKucukAdres.Name = "txtKucukAdres";
+            this.txtKucukAdres.Size = new System.Drawing.Size(577, 72);
+            this.txtKucukAdres.TabIndex = 2;
+            // 
+            // AdresDonusturucu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 214);
+            this.Controls.Add(this.txtKucukAdres);
+            this.Controls.Add(this.txtBuyukAdres);
+            this.Controls.Add(this.txtAdres);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdresDonusturucu";
+            this.Text = "Adres Dönüştürücü";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtBuyukAdres;
+        private System.Windows.Forms.TextBox txtKucukAdres;
+    }
+}
+
---
+++ AdresDonusturucu/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30_MetinCevirici
+{
+    public partial class AdresDonusturucu : Form
+    {
+        public AdresDonusturucu()
+        {
+            InitializeComponent();
+        }
+        private void txtAdres_TextChanged(object sender, EventArgs e)
+        {
+            txtBuyukAdres.Text = ConvertTurkishCharactersToEnglishUpper(RemoveExtraSpaces(txtAdres.Text)).ToUpper();
+            txtKucukAdres.Text = ConvertTurkishCharactersToEnglishLower(RemoveExtraSpaces(txtAdres.Text)).ToLower();
+        }
+        static string ConvertTurkishCharactersToEnglishUpper(string input)
+        {
+            string output = input;
+            output = output.Replace("ç", "C");
+            output = output.Replace("ğ", "G");
+            output = output.Replace("ı", "I");
+            output = output.Replace("i", "I");
+            output = output.Replace("ö", "O");
+            output = output.Replace("ş", "S");
+            output = output.Replace("ü", "U");
+            output = output.Replace("Ç", "C");
+            output = output.Replace("Ğ", "G");
+            output = output.Replace("I", "I");
+            output = output.Replace("İ", "I");
+            output = output.Replace("Ö", "O");
+            output = output.Replace("Ş", "S");
+            output = output.Replace("Ü", "U");
+            return output;
+        }
+        static string ConvertTurkishCharactersToEnglishLower(string input)
+        {
+            string output = input;
+            output = output.Replace("ç", "c");
+            output = output.Replace("ğ", "g");
+            output = output.Replace("ı", "i");
+            output = output.Replace("ö", "o");
+            output = output.Replace("ş", "s");
+            output = output.Replace("ü", "u");
+            output = output.Replace("Ç", "c");
+            output = output.Replace("Ğ", "g");
+            output = output.Replace("I", "i");
+            output = output.Replace("İ", "i");
+            output = output.Replace("Ö", "o");
+            output = output.Replace("Ş", "s");
+            output = output.Replace("Ü", "u");
+            return output;
+        }
+        static string RemoveExtraSpaces(string input)
+        {
+            return Regex.Replace(input, @"\s+", " ");
+        }
+    }
+}
